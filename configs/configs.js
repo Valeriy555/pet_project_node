ea3dad6711fee08022959645f9c9381d23743d3f
Valeriy555 <sokolchenkov@gmail.com>
2023-03-29 23:17:06 +0300

    cloud mongoDB

--- configs/configs.js
+++ configs/configs.js
@@ -1,4 +1,5 @@
 module.exports = {
     PORT: process.env.PORT || 5003,
-    MONGO_URL:process.env.MONGO_URL || 'mongodb://127.0.0.1/petProject'
+    // MONGO_URL:process.env.MONGO_URL || 'mongodb://127.0.0.1/petProject'
+    MONGO_URL:process.env.MONGO_URL || 'mongodb+srv://valera:holenko@cluster0.g24o5a1.mongodb.net/test'
 }
